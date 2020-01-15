326772aa58f1b423c7cf7be17ee42d42f3d57440
Logan Lim <Logan.Lim@cra-arc.gc.ca>
2020-01-15 07:46:01 -0500

    resume parsing now functional

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_cors import CORS
 
-UPLOAD_FOLDER = 'C:/resumeUpload'
+UPLOAD_FOLDER = 'C:/resumeUpload/'
 
 app = Flask(__name__)
 cors = CORS(app)
--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
+import os
 from app import app
-from flask import request, jsonify
+from flask import request, jsonify, url_for
 from flask_cors import cross_origin
 from werkzeug.utils import secure_filename
 
@@ -8,12 +9,13 @@ from app import processor
 @app.route('/process', methods = ['POST'])
 @cross_origin()
 def process():
-    file = request.files['file']
+    file = request.files['fileKey']
     filename = secure_filename(file.filename)
-    file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-    result = processor.process(url_for('uploaded_file', filename=filename))
-    merged_dict = {key: value for (key, value) in (result.items() + request.get_json())}
-    return jsonify(merged_dict)
+    filePath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+    file.save(filePath)
+    result = processor.process(filePath)
+    print('result:', result)
+    return result
 
 def allowed_file(filename):
     return '.' in filename and \
