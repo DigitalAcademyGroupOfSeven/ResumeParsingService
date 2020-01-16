c042b3ac3061a6be32acc968ff7c3ce72c2e89aa
Logan-Lim <Logan.Lim@cra-arc.gc.ca>
2020-01-15 19:48:00 -0500

    added setup.py file

---
+++ setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name="resume-parsing-service",
+    version="0.0.1",
+    author="GroupOfSeven",
+    author_email="Logan.Lim@cra-arc.gc.ca",
+    description="Python service to parse resumes",
+    package=setuptools.find_packages()
+)
